69c46d20df59a1a85a520fa829207742f56a6de3
JounQin <admin@1stg.me>
2022-05-30 22:17:25 +0800

    fix(api): wrap data into json

--- api/translate.ts
+++ api/translate.ts
@@ -2,45 +2,72 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 import type { SourceLanguage, TargetLanguage } from 'deepl-translate'
 
 // Workaround for Vercel `Cannot find module 'deepl-translate'`
-import { translate } from './_deepl'
+import { abbreviateLanguage, translate } from './_deepl'
 
 export interface RequestParams {
-  text: string
+  text?: string
   source_lang?: SourceLanguage
   target_lang: TargetLanguage
 }
 
+const OK = 200
 const NO_CONTENT = 204
 const NOT_ALLOWED = 405
+const INTERNAL_ERROR = 500
 
 export default async (
   req: VercelRequest,
   res: VercelResponse,
 ): Promise<void> => {
-  if (req.method !== 'POST') {
-    res.status(NOT_ALLOWED)
-    res.end()
-    return
-  }
-
-  const {
+  let {
     text,
     source_lang: sourceLang,
     target_lang: targetLang,
     // type-coverage:ignore-next-line
   } = req.body as RequestParams
 
-  if (!text) {
+  if (req.method !== 'POST' || !abbreviateLanguage(targetLang)) {
+    res.status(NOT_ALLOWED)
+    res.end(
+      JSON.stringify({
+        code: NOT_ALLOWED,
+      }),
+    )
+    return
+  }
+
+  if (text == null) {
     res.status(NO_CONTENT)
-    res.end()
+    return
+  }
+
+  text = text.trim()
+
+  if (!text) {
+    res.end(
+      JSON.stringify({
+        code: OK,
+        data: '',
+      }),
+    )
     return
   }
 
   try {
     const translated = await translate(text, targetLang, sourceLang)
-    res.end(translated)
+    res.end(
+      JSON.stringify({
+        code: OK,
+        data: translated,
+      }),
+    )
   } catch (err) {
-    res.status(500)
-    res.end(String(err))
+    res.status(INTERNAL_ERROR)
+    res.end(
+      JSON.stringify({
+        code: INTERNAL_ERROR,
+        data: String(err),
+      }),
+    )
   }
 }
